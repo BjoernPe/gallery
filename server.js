d188fb05ff8529dcacdf946f563001c8f1b540ba
BjoernPe <bjoern.pe@t-online.de>
2023-12-06 10:54:57 +0100

    Server

--- server.js
+++ server.js
@@ -2,13 +2,12 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs').promises;
 const multer = require('multer');
+const cors = require('cors'); // CORS-Modul hinzugefügt
 
 const app = express();
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3000; // Dynamischer Port für Netlify
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Multer-Konfiguration für den Dateiupload
+// Middleware für den Dateiupload
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, path.join(__dirname, 'public', 'images', 'upload', 'hongkong'));
@@ -19,6 +18,12 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage: storage });
+
+// CORS aktivieren
+app.use(cors());
+
+// Statisches Verzeichnis für die Client-Anwendung
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Endpoint zum Lesen der Bilddateien
 app.get('/images', async (req, res) => {
@@ -44,7 +49,27 @@ app.post('/upload', upload.array('images'), (req, res) => {
   res.json({ message: 'Upload successful' });
 });
 
-app.get('/images', async (req, res) => {
+// Endpoint zum Löschen von Bildern
+app.delete('/delete', async (req, res) => {
+  try {
+    const imagePath = req.query.path;
+    const absolutePath = path.join(__dirname, 'public', 'images', 'upload', 'hongkong', path.basename(imagePath));
+    console.log('Bild:', absolutePath);
+    await fs.unlink(absolutePath);
+    
+    res.json({ success: true, message: 'Bild erfolgreich gelöscht' });
+  } catch (error) {
+    console.error('Bild:',  req.query.path);
+    console.error('Fehler beim Löschen des Bildes:', error);
+    res.status(500).json({ success: false, message: 'Interner Serverfehler' });
+  }
+});
+
+// Alle anderen Anfragen werden zur index.html weitergeleitet
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
